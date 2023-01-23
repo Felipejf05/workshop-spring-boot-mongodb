60169e67decc8a53b72f13fe7774a5f7edd1ab04
Felipe <jorgefelipe.jf01@gmail.com>
2023-01-23 20:14:10 -0300

    Consulta simples com query methods

--- src/main/java/com/felipejf05/workshopmongo/respository/PostRepository.java
+++ src/main/java/com/felipejf05/workshopmongo/respository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.felipejf05.workshopmongo.respository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.felipejf05.workshopmongo.domain.Post;
 
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
